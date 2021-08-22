510c57eea0e522d1a70289b110b2ae19d44e59f0
Akash-gowda-hr <akashhr2040@gmail.com>
2021-08-22 08:12:24 +0530

    ADDED:even odd checker

---
+++ src/com/bridgelabz/basiccoreprogramme/EvenOddChecker.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.basiccoreprogramme;
+
+public class EvenOddChecker {
+	public static void main(String[] args ) {
+		int numberToCheck = 1;
+		if(numberToCheck % 2 == 0) {
+			System.out.println("The number : "+  numberToCheck + " is a even number!!!!.");
+		}
+		else{
+			System.out.println("The number : "+ numberToCheck + " is a odd number!!!.");
+		}
+	}
+}
