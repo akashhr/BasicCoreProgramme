a2aff9907a65913db1f2b08f7c0c92e036927b7d
Akash-gowda-hr <akashhr2040@gmail.com>
2021-08-22 18:41:48 +0530

    MODIFIED:Datatype of the number

--- src/com/bridgelabz/basiccoreprogramme/QuotientRemainderCalculator.java
+++ src/com/bridgelabz/basiccoreprogramme/QuotientRemainderCalculator.java
@@ -3,15 +3,15 @@ package com.bridgelabz.basiccoreprogramme;
 public class QuotientRemainderCalculator {
 
 	public static void main(String[] args) {
-		final int divisor = 10;
-		final int dividend = -100;
+		 double divisor = 10;
+		 double dividend = 101;
 		
 		if(divisor == 0) {
 			System.err.println("Divisor cannot be zero!!!!");
 			System.exit(0);
 		}else {
-			int remainder = dividend % divisor ; 
-			int quotient = dividend / divisor;
+			int remainder = (int) (dividend % divisor) ; 
+			double quotient = dividend / divisor;
 			System.out.println("Remainder is: "+ remainder);
 			System.out.println("Quotient is :" +quotient);
 			
